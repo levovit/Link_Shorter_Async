6d034d67e454ccf9fca4fadabea6ff1db8054390
levovit <playersoft1999@gmail.com>
2019-12-01 05:03:36 +0200

    completly new design

--- app/static/js/ajax.js
+++ app/static/js/ajax.js
@@ -1,25 +1,26 @@
 "use strict";
 
- $(function() {
-     $('#submitButton').click(function() {
-         const url = $("#url").val();
-         let days = $("#days").val();
-         days = days ? days : "90";
-         $.ajax({
-             type: "POST",
-             url: "/short",
-             data: JSON.stringify({"url" : url, "days": days,}),
-             success: returnSuccess,
-             error: returnError,
-             dataType: "json",
-             contentType: "application/json",
-         });
-     });
+$(function () {
+    $('#submitButton').click(function () {
+        const url = $("#url").val();
+        let days = $("#days").val();
+        days = days ? days : "90";
+        $.ajax({
+            type: "POST",
+            url: "/short",
+            data: JSON.stringify({"url": url, "days": days,}),
+            success: returnSuccess,
+            error: returnError,
+            dataType: "json",
+            contentType: "application/json",
+        });
+    });
 });
 
 function returnSuccess(data, textStatus, jqXHR) {
-    if(data.url) {
+    if (data.url) {
         $('#url-result').text(data.url);
+        $('#url-result').attr('class', 'display-4 text-success');
         $('#url').val("");
         $("a").attr("href", data.url);
     } else {
@@ -27,13 +28,14 @@ function returnSuccess(data, textStatus, jqXHR) {
     }
 }
 
-function returnError(jqXHR, textStatus, errorThrown ) {
+function returnError(jqXHR, textStatus, errorThrown) {
     console.log(errorThrown);
     console.log(textStatus);
     console.log(jqXHR);
     console.log(typeof errorThrown);
     $('#url-result').text(jqXHR.responseJSON.error.message);
+    $('#url-result').attr('class', 'display-4 text-danger');
+
     $('#url').val("");
     $("a").attr("href", "#");
-
 }
